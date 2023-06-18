60d42006831bfc7795f7af2df27ad329c3dbc485
Pavel Emelianov <pavel.emelyanov.dev@gmail.com>
2023-06-18 15:08:13 +0700

    add files in system count in header

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,25 @@ import { SearchBar, FilesTree, Header } from 'components'
 import { setInitialTheme } from 'utils'
 
 import styles from './App.module.css'
-import filesStore from 'store/filesStore'
-import { observer } from 'mobx-react-lite'
 
-// add total items count on top of the page
-// increase items to 10k
 // fix of sorting top layer and search results (do not sort)
 // tests
 
 // Features later: move files (to other folders)
 
-const App: React.FC = observer(() => {
+const App: React.FC = () => {
   useEffect(() => {
     setInitialTheme()
   }, [])
+
   return (
     <>
       <Header />
-      <p>files in system: {filesStore.totalFilesCount}</p>
       <h1 className={styles.h1}>Search files you need</h1>
       <SearchBar />
       <FilesTree />
     </>
   )
-})
+}
 
 export default App
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import cx from 'classnames'
+import { observer } from 'mobx-react-lite'
 import { getTheme, setTheme as setStorageTheme } from 'utils'
+import filesStore from 'store/filesStore'
 
 import styles from './Header.module.css'
 
 import { ReactComponent as SunIcon } from './assets/sun.svg'
 import { ReactComponent as MoonIcon } from './assets/moon.svg'
 
-export const Header: React.FC = () => {
+export const Header: React.FC = observer(() => {
   const [theme, setTheme] = useState<Theme>(getTheme())
 
   const setDarkTheme = () => {
@@ -21,6 +23,7 @@ export const Header: React.FC = () => {
 
   return (
     <header className={styles.header}>
+      <p className={styles.filesInSystem}>Files in system: {filesStore.totalFilesCount}</p>
       <button onClick={setDarkTheme} className={styles.modeButton}>
         <MoonIcon
           className={cx({
@@ -39,4 +42,4 @@ export const Header: React.FC = () => {
       </button>
     </header>
   )
-}
+})
